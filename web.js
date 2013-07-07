a4494e393050e77eb19584be08407eee2eb40fd5
mikeyindev <mikeyindev@gmail.com>
2013-07-07 06:28:50 +0000

    LAST TRY

--- web.js
+++ web.js
@@ -2,16 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World from index.html');
+app.get('/index.html', function(request, response) {
+	//response.send(Hello World!);
 });
 
 //var fs = require('fs');
-//var readFile = "index.html";
-//fs.writeFileSync(readFile);
-
-//console.log("Hello World from" + " " + readFile);
-
+//var http = require('http');
+//var index = fs.readFileSync('index.html'); 
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
