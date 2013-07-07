9dec01f753885b6e57f4d2ae22cdee5ccd2ffe13
mikeyindev <mikeyindev@gmail.com>
2013-07-07 05:41:53 +0000

    modified web.js

--- web.js
+++ web.js
@@ -3,17 +3,17 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('Hello World from index.html');
 });
 
-var fs = require('fs');
-var readFile = "index.html";
-fs.writeFileSync(readFile);
+//var fs = require('fs');
+//var readFile = "index.html";
+//fs.writeFileSync(readFile);
 
-console.log("Hello World from" + " " + readFile);
+//console.log("Hello World from" + " " + readFile);
 
 
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-  //console.log("Listening on " + port);
-	//});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
