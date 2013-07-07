93be1d20333ed65183b7b4e10e40809fe9dbe7c2
mikeyindev <mikeyindev@gmail.com>
2013-07-07 04:52:03 +0000

    Added writeFileSync to web.js

--- web.js
+++ web.js
@@ -6,7 +6,14 @@ app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+var fs = require('fs');
+var readFile = "index.html";
+fs.writeFileSync(readFile);
+
+console.log("Hello World from" + " " + readFile);
+
+
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+  //console.log("Listening on " + port);
+	//});
