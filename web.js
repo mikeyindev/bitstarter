d22fc6af6f17fcb282b7ccd89cfcc39aa7572a07
mikeyindev <mikeyindev@gmail.com>
2013-07-07 07:05:41 +0000

    Fail Faster

--- web.js
+++ web.js
@@ -1,14 +1,31 @@
-var express = require('express');
+//var express = require('express');
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
 
-app.get('/index.html', function(request, response) {
+//app.get('/index.html', function(request, response) {
 	//response.send(Hello World!);
-});
+	//});
 
 //var fs = require('fs');
 //var http = require('http');
 //var index = fs.readFileSync('index.html'); 
+
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (request, response)) {
+	console.log('request starting...');
+	
+	fs.readFile('./index.html', function(error, content)) {
+		if(error) {
+			response.writeHead(500);
+			response.end(;)
+		}
+		else {
+			response.writeHEad(200, {'Content-Type': 'text/html'});
+			response.end(content, 'utf-8');
+		}
+	}
+};
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
